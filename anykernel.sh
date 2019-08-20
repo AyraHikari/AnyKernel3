0681fc5eab0f7e09cf4d00fdf0ee9d76a7358681
Ayra Hikari <ayrahikari@linuxmail.org>
2019-08-20 13:06:22 +0700

    Added volume key selection
    To disable or enable by default for dt2w.
    Many peoples ask for this

--- anykernel.sh
+++ anykernel.sh
@@ -29,6 +29,98 @@ ramdisk_compression=auto;
 . tools/ak3-core.sh;
 
 
+# Get option from zip name if applicable
+case $(basename $ZIP) in
+  *new*|*New*|*NEW*) NEW=true;;
+  *old*|*Old*|*OLD*) NEW=false;;
+esac
+
+# Change this path to wherever the keycheck binary is located in your installer
+KEYCHECK=$INSTALLER/keycheck
+chmod 755 $KEYCHECK
+
+keytest() {
+  ui_print "- Vol Key Test -"
+  ui_print "   Press a Vol Key:"
+  (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || return 1
+  return 0
+}   
+
+choose() {
+  #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+  while true; do
+    /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+      break
+    fi
+  done
+  if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+    return 0
+  else
+    return 1
+  fi
+}
+
+chooseold() {
+  # Calling it first time detects previous input. Calling it second time will do what we want
+  $KEYCHECK
+  $KEYCHECK
+  SEL=$?
+  if [ "$1" == "UP" ]; then
+    UP=$SEL
+  elif [ "$1" == "DOWN" ]; then
+    DOWN=$SEL
+  elif [ $SEL -eq $UP ]; then
+    return 0
+  elif [ $SEL -eq $DOWN ]; then
+    return 1
+  else
+    ui_print "   Vol key not detected!"
+    abort "   Use name change method in TWRP"
+  fi
+}
+ui_print " "
+if [ -z $NEW ]; then
+  if keytest; then
+    FUNCTION=choose
+  else
+    FUNCTION=chooseold
+    ui_print "   ! Legacy device detected! Using old keycheck method"
+    ui_print " "
+    ui_print "- Vol Key Programming -"
+    ui_print "   Press Vol Up Again:"
+    $FUNCTION "UP"
+    ui_print "   Press Vol Down"
+    $FUNCTION "DOWN"
+  fi
+  ui_print " "
+  ui_print "- Select Option -"
+  ui_print " "
+  ui_print "   Do you want to enable dt2w by default?"
+  ui_print " "
+  ui_print "   Vol Up   =  Yes"
+  ui_print "   Vol Down =  No"
+  if $FUNCTION; then 
+    NEW=true
+  else 
+    NEW=false
+  fi
+else
+  ui_print "   Option specified in zipname!"
+fi
+
+if $NEW; then
+  ui_print " "
+  ui_print "- DT2W now enabled by default..."
+  cp tools/yuka/Edt2w/* $ramdisk/
+  
+else
+  ui_print " "
+  ui_print "- DT2W now disabled by default..."
+  cp tools/yuka/Ddt2w/* $ramdisk/
+fi
+
+
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
